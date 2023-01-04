9102333d877b36a6d35e951906ee35b723e4fa63
Bo McCarthy <sysrock@gmail.com>
2023-01-04 16:10:28 -0600

    changed Home.js email.

--- src/views/Home.js
+++ src/views/Home.js
@@ -23,7 +23,7 @@ export default class Home extends Component {
                         <div className='home-text-style'>
                             <p>Thank you for visiting the Cigar Database. This is a work in progress to catalog all the cigars in the world. There will be mistakes, but with a little crowdsourcing, perhaps we can all have something to help others bring new things to our passion.</p>
                             <p>For a little help with this, the "Database" in the navigation bar at the top of the page is the entire database of more than 56,000 cigars. It takes time to load, so be aware that it may not be the fastest way to get information. Please feel free to use the search functions in the "Filter" page to whittle down the database to a manageable size.</p>
-                            <p>Should you find a mistake or a correction that should be made, please send an email to <a href='mailto:my.help@cigar-db.com'>my.help@cigar-db.com</a>. I will try to make the corrections in a timely manner.</p>
+                            <p>Should you find a mistake or a correction that should be made, please send an email to <a href='mailto:sysrock@gmail.com'>sysrock@gmail.com</a>. I will try to make the corrections in a timely manner.</p>
                             <p>Thanks again and enjoy your time on the Cigar Database...</p>
                         </div>
                     </div>
