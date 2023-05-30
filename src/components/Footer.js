bab45bf5a87fea600bd129c8f5a7b80e3a1506bb
bomccarthy <110850293+bomccarthy@users.noreply.github.com>
2023-05-30 13:23:38 -0500

    Update Footer.js mailto Add Subject, Open New Tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ export default class Footer extends Component {
                 <div className="container-fluid justify-content-end">
                     <div className="navbar-nav">
                         <Link className="nav-link active mx-2" to="/">Cigar DB</Link>
-                        <a className="nav-link active mx-2" href={'mailto:sysrock@gmail.com'}>Contact</a>
+                        <a className="nav-link active mx-2" target="_blank" rel="noopener noreferrer" href={'mailto:sysrock@gmail.com?subject=Cigar-db.com Contact'}>Contact</a>
                         <Link className="nav-link active mx-2" to="/about">About</Link>
                     </div>
                 </div>
