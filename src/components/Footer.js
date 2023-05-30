4b58e6c8d8343edca1fe73f04ae1c6e45a0a9feb
bomccarthy <110850293+bomccarthy@users.noreply.github.com>
2023-05-30 13:08:45 -0500

    Update Footer.js mailto

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ export default class Footer extends Component {
                 <div className="container-fluid justify-content-end">
                     <div className="navbar-nav">
                         <Link className="nav-link active mx-2" to="/">Cigar DB</Link>
-                        <Link className="nav-link active mx-2" to="mailto:sysrock@gmail.com">Contact</Link>
+                        <a className="nav-link active mx-2" href={'mailto:sysrock@gmail.com'}>Contact</a>
                         <Link className="nav-link active mx-2" to="/about">About</Link>
                     </div>
                 </div>
